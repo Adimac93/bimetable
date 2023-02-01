9eb83f10dbc6357ee8afdec0d67306b88be117ca
Adimac93 <adimac93@gmail.com>
2023-02-01 13:29:17 +0100

    ✅ normalise fixtures SQL syntax

--- backend/tests/fixtures/users.sql
+++ backend/tests/fixtures/users.sql
@@ -1,13 +1,13 @@
-insert into users (id, username)
-values
+INSERT INTO users (id, username)
+VALUES
 ('32190025-7c15-4adb-82fd-9acc3dc8e7b6','mabi19'),
 ('29e40c2a-7595-42d3-98e8-9fe93ce99972','pkb-pmj'),
 ('910e81a9-56df-4c24-965a-13eff739f469','adimac93'),
 ('a9c5900e-a445-4888-8612-4a5c8cadbd9e','hubertk');
 
 -- password #strong#_#pass#
-insert into credentials (user_id, login, password)
-values
+INSERT INTO credentials (user_id, login, password)
+VALUES
 ('32190025-7c15-4adb-82fd-9acc3dc8e7b6','mabmab','$argon2i$v=19$m=4096,t=3,p=1$M0g3ODVzWmQ$fHLpcolZURzJzej/xbDQqTb+OINmUOl8uEFVLah0z8Y'),
 ('29e40c2a-7595-42d3-98e8-9fe93ce99972','pkbpkp','$argon2i$v=19$m=4096,t=3,p=1$M0g3ODVzWmQ$fHLpcolZURzJzej/xbDQqTb+OINmUOl8uEFVLah0z8Y'),
 ('910e81a9-56df-4c24-965a-13eff739f469','macmac','$argon2i$v=19$m=4096,t=3,p=1$M0g3ODVzWmQ$fHLpcolZURzJzej/xbDQqTb+OINmUOl8uEFVLah0z8Y'),
