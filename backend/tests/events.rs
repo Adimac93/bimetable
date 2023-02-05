2d146322f0f591ab5d90071492882f515ec69a93
Adimac93 <adimac93@gmail.com>
2023-02-05 15:14:39 +0100

    ✅ adapt to new event format

--- backend/tests/events.rs
+++ backend/tests/events.rs
@@ -1,6 +1,7 @@
-use bimetable::routes::events::models::{CreateEvent, Event, GetEventsQuery};
+use bimetable::{routes::events::models::{CreateEvent, GetEventsQuery}, utils::events::models::{EventRules, TimeRules, EndsAt, Event}};
 use http::StatusCode;
-use sqlx::PgPool;
+use serde_json::json;
+use sqlx::{PgPool, query, query_as};
 use time::macros::datetime;
 use tracing_test::traced_test;
 use uuid::{uuid, Uuid};
@@ -10,59 +11,84 @@ use crate::tools::AppData;
 mod tools;
 
 #[traced_test]
-#[sqlx::test]
-async fn create_event(pool: PgPool) {
-    let app = AppData::new(pool).await;
-    let client = app.client();
+#[sqlx::test(fixtures("users"))]
+fn create_event(pool: PgPool) {
+    let user_id = uuid!("32190025-7c15-4adb-82fd-9acc3dc8e7b6");
+    let body = CreateEvent {
+    name: "abc".into(), 
+    starts_at: Some(datetime!(2023-02-05 10:00 +1)), 
+    ends_at: Some(datetime!(2023-02-05 12:00 +1)), 
+    recurrence_rule: Some(EventRules::Weekly { time_rules: TimeRules {ends_at: Some(EndsAt::Until(datetime!(2040-02-05 12:00 +1))), interval: 1}, week_map: 0b0010011 })};
+    let rec = query_as!(
+        Event,
+        r#"
+            INSERT INTO events (name, owner_id, starts_at, ends_at , recurrence_rule)
+            VALUES
+            ($1, $2, $3, $4, $5)
+            RETURNING id, owner_id, name, starts_at, ends_at, recurrence_rule as "recurrence_rule: _";
+        "#,
+        body.name,
+        user_id,
+        body.starts_at,
+        body.ends_at,
+        sqlx::types::Json(body.recurrence_rule) as _
+    )
+    .fetch_one(&pool)
+    .await.unwrap();
 
-    let req = CreateEvent {
-        starts_at: datetime!(2023-02-06 8:00 +1),
-        ends_at: datetime!(2023-02-06 8:45 +1),
-        name: "Matematyka".into(),
-    };
-
-    let res = client
-        .put(app.api("/events"))
-        .json(&req)
-        .send()
-        .await
-        .unwrap();
-
-    assert_eq!(res.status(), StatusCode::CREATED);
-
-    let _res: Uuid = res.json().await.unwrap();
+    println!("{rec:#?}");
 }
 
+
 #[traced_test]
-#[sqlx::test(fixtures("events"))]
+#[sqlx::test(fixtures("users", "events"))]
 async fn get_events_in_time_range(pool: PgPool) {
     let app = AppData::new(pool).await;
     let client = app.client();
 
     let query = GetEventsQuery {
-        starts_at: datetime!(2023-02-06 8:00 +1),
-        ends_at: datetime!(2023-02-06 9:35 +1),
+        starts_at: Some(datetime!(2023-02-06 8:00 +1)),
+        ends_at: Some(datetime!(2023-02-06 9:35 +1)),
     };
 
+    let payload = json!({
+        "login": "pkbpkp",
+        "password": "#strong#_#pass#",
+    });
+
+    let res = client
+        .post(app.api("/auth/login"))
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(res.status(), StatusCode::OK);
+        
     let res = client
         .get(app.api("/events"))
         .query(&query)
         .send()
         .await
         .unwrap();
+
     assert_eq!(res.status(), StatusCode::OK);
 
     let expected = vec![
         Event {
             id: uuid!("248c5f26-e48e-4ada-bace-384b1badb95c"),
-            starts_at: datetime!(2023-02-06 8:00 +1),
-            ends_at: datetime!(2023-02-06 8:45 +1),
+            owner_id: uuid!("29e40c2a-7595-42d3-98e8-9fe93ce99972"),
+            starts_at: Some(datetime!(2023-02-06 8:00 +1)),
+            ends_at: Some(datetime!(2023-02-06 8:45 +1)),
+            recurrence_rule: None,
             name: "Matematyka".into(),
         },
         Event {
             id: uuid!("73cb2256-80ce-4c0b-b753-34fdd2c7f5e5"),
-            starts_at: datetime!(2023-02-06 8:50 +1),
-            ends_at: datetime!(2023-02-06 9:35 +1),
+            owner_id: uuid!("29e40c2a-7595-42d3-98e8-9fe93ce99972"),
+            starts_at: Some(datetime!(2023-02-06 8:50 +1)),
+            ends_at: Some(datetime!(2023-02-06 9:35 +1)),
+            recurrence_rule: None,
             name: "Fizyka".into(),
         },
     ];
--- backend/tests/fixtures/events.sql
+++ backend/tests/fixtures/events.sql
@@ -1,4 +1,4 @@
-INSERT INTO events (id, starts_at, ends_at, name)
+INSERT INTO events (id, owner_id, name, starts_at, ends_at)
 VALUES
-('248c5f26-e48e-4ada-bace-384b1badb95c', TIMESTAMPTZ '2023-02-06 08:00 +1', TIMESTAMPTZ '2023-02-06 08:45 +1', 'Matematyka'),
-('73cb2256-80ce-4c0b-b753-34fdd2c7f5e5', TIMESTAMPTZ '2023-02-06 08:50 +1', TIMESTAMPTZ '2023-02-06 09:35 +1', 'Fizyka');
+('248c5f26-e48e-4ada-bace-384b1badb95c', '29e40c2a-7595-42d3-98e8-9fe93ce99972', 'Matematyka', TIMESTAMPTZ '2023-02-06 08:00 +1', TIMESTAMPTZ '2023-02-06 08:45 +1'),
+('73cb2256-80ce-4c0b-b753-34fdd2c7f5e5', '29e40c2a-7595-42d3-98e8-9fe93ce99972', 'Fizyka', TIMESTAMPTZ '2023-02-06 08:50 +1', TIMESTAMPTZ '2023-02-06 09:35 +1');
