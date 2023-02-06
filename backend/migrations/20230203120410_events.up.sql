927a2fc817608d35a14154f75a0c7c6306bb1723
HubertK05 <hubert.klimowicz.hk@gmail.com>
2023-02-06 20:32:58 +0100

    🗃️ modified events model
    Co-Authored-By: Adimac93 <47300834+Adimac93@users.noreply.github.com>

--- backend/migrations/20230203120410_events.up.sql
+++ backend/migrations/20230203120410_events.up.sql
@@ -3,11 +3,24 @@ CREATE TABLE events
     id                 UUID DEFAULT gen_random_uuid(),
     owner_id           UUID NOT NULL,
     name               TEXT NOT NULL,
+    description        TEXT NOT NULL,
     starts_at          TIMESTAMPTZ,
     ends_at            TIMESTAMPTZ,
     recurrence_rule    JSONB,
     PRIMARY KEY (id),
     FOREIGN KEY (owner_id) REFERENCES users (id)
+);
+
+CREATE TABLE modified_events
+(
+    origin_id        UUID,
+    occurrence_id    INT,
+    name             TEXT NOT NULL,
+    description      TEXT NOT NULL,
+    starts_at        TIMESTAMPTZ,
+    ends_at          TIMESTAMPTZ,
+    PRIMARY KEY (origin_id, occurrence_id),
+    FOREIGN KEY (origin_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_events
