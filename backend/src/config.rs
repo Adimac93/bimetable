3d4c10215c3e9e28e29288478d5a684e3d3649a8
Adimac93 <adimac93@gmail.com>
2023-02-14 20:32:50 +0100

    🧑‍💻 default config parts ~ config is now optional for development environment

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -2,13 +2,80 @@ use config::{Config, ConfigError};
 use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 use std::net::SocketAddr;
-use tracing::info;
+use tracing::{error, info, warn};
+
+const CONFIG_DIR: &str = "configuration";
+const CONFIG_FILE_NAME: &str = "settings.toml";
 
 #[derive(Deserialize, Clone)]
+pub struct SettingsModel {
+    pub app: Option<ApplicationSettings>,
+    pub jwt: Option<JwtSettings>,
+    pub postgres: Option<PostgresSettings>,
+}
+
+impl SettingsModel {
+    fn parse() -> Result<Self, ConfigError>{
+        let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+        let config_dir = base_path.join(CONFIG_DIR);
+        let settings = Config::builder()
+            .add_source(config::File::from(config_dir.join(CONFIG_FILE_NAME)))
+            .add_source(
+                config::Environment::with_prefix("APP")
+                    .prefix_separator("_")
+                    .separator("__"),
+            );
+        Ok(settings.build()?.try_deserialize()?)
+    }
+}
+
+#[derive(Clone)]
 pub struct Settings {
     pub app: ApplicationSettings,
     pub jwt: JwtSettings,
     pub postgres: PostgresSettings,
+}
+
+impl Settings {
+    fn dev(model: Option<SettingsModel>) -> Self {
+        if let Some(model) = model {
+            let app = model.app.unwrap_or_else(|| {
+                warn!("Using default `app` settings!");
+                ApplicationSettings::default()
+            });
+            let jwt = model.jwt.unwrap_or_else(|| {
+                warn!("Using default `jwt` settings!");
+                JwtSettings::default()
+            });
+            let postgres = model.postgres.unwrap_or_else(|| {
+                let settings = PostgresSettings::default();
+                warn!("Using default `postgres` settings (env url)!");
+                settings
+            });
+            return Self {app,jwt,postgres};
+        }
+        let default = Self::default();
+        warn!("Using default configuration!");
+        default
+    }
+
+    fn prod() -> Self {
+        Self {
+            app: ApplicationSettings::from_env(),
+            jwt: JwtSettings::from_env(),
+            postgres: PostgresSettings::from_env(),
+        }
+    }
+}
+
+impl Default for Settings {
+    fn default() -> Self {
+        let app = ApplicationSettings::default();
+        let jwt = JwtSettings::default();
+        let postgres = PostgresSettings::default();
+
+        Self {app,jwt,postgres}
+    }
 }
 
 #[derive(Deserialize, Clone)]
@@ -34,6 +101,16 @@ impl ApplicationSettings {
     }
 }
 
+impl Default for ApplicationSettings {
+    fn default() -> Self {
+        Self {
+            host: "127.0.0.1".to_string(),
+            port: 3001,
+            origin: "http://127.0.0.1".to_string()
+        }
+    }
+}
+
 #[derive(Deserialize, Clone)]
 pub struct JwtSettings {
     pub access_secret: Secret<String>,
@@ -45,6 +122,15 @@ impl JwtSettings {
         Self {
             access_secret: get_secret_env("JWT_ACCESS_SECRET"),
             refresh_secret: get_secret_env("JWT_REFRESH_SECRET"),
+        }
+    }
+}
+
+impl Default for JwtSettings {
+    fn default() -> Self {
+        Self {
+            access_secret: Secret::new("ACCESS_SECRET".to_string()),
+            refresh_secret: Secret::new("REFRESH_SECRET".to_string()),
         }
     }
 }
@@ -116,6 +202,16 @@ impl PostgresSettings {
     }
 }
 
+impl Default for PostgresSettings {
+    fn default() -> Self {
+        Self {
+            database_url: Self::env_database_url(),
+            fields: None,
+            is_migrating: Some(false),
+        }
+    }
+}
+
 impl ToString for PostgresSettings {
     fn to_string(&self) -> String {
         String::from("postgresql")
@@ -135,7 +231,7 @@ impl ConnectionPrep for PostgresSettings {
 }
 
 enum Environment {
-    Local,
+    Development,
     Production,
 }
 
@@ -144,8 +240,8 @@ impl TryFrom<String> for Environment {
 
     fn try_from(value: String) -> Result<Self, Self::Error> {
         match value.to_lowercase().as_str() {
-            "local" => Ok(Self::Local),
-            "production" => Ok(Self::Production),
+            "development"|"dev"|"local" => Ok(Self::Development),
+            "production"|"prod"|"remote" => Ok(Self::Production),
             other => Err(format!(
                 "{other} is not supported environment. Use either `local` or `production`"
             )),
@@ -153,36 +249,22 @@ impl TryFrom<String> for Environment {
     }
 }
 
-pub fn get_config() -> Result<Settings, ConfigError> {
-    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
-    let config_dir = base_path.join("configuration");
-
+pub fn get_config() -> Result<Settings, anyhow::Error> {
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
-        .map_or(Environment::Local, |env| {
+        .map_or(Environment::Development, |env| {
             env.try_into().expect("Failed to parse APP_ENVIRONMENT.")
         });
 
-    match environment {
-        Environment::Local => {
-            let settings = Config::builder()
-                .add_source(config::File::from(config_dir.join("settings.toml")))
-                .add_source(
-                    config::Environment::with_prefix("APP")
-                        .prefix_separator("_")
-                        .separator("__"),
-                );
-            return settings.build()?.try_deserialize();
+    return match environment {
+        Environment::Development => {
+            let res = SettingsModel::parse().map_err(|e| error!("{e}\n - check {CONFIG_DIR}/{CONFIG_FILE_NAME}, reference at README.md"));
+            Ok(Settings::dev(res.ok()))
         }
 
         Environment::Production => {
-            let settings = Settings {
-                app: ApplicationSettings::from_env(),
-                jwt: JwtSettings::from_env(),
-                postgres: PostgresSettings::from_env(),
-            };
-            return Ok(settings);
+            Ok(Settings::prod())
         }
-    }
+    };
 }
 
 fn try_get_env(name: &str) -> Option<String> {
