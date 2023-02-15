ca1c0195460e826eb153402150a284392bf3d225
Adimac93 <adimac93@gmail.com>
2023-02-15 21:45:43 +0100

    🚧🗃️ event overrides + override range

--- backend/migrations/20230203120410_events.up.sql
+++ backend/migrations/20230203120410_events.up.sql
@@ -1,14 +1,28 @@
 CREATE TABLE events
 (
-    id                 UUID DEFAULT gen_random_uuid(),
-    owner_id           UUID NOT NULL,
-    name               TEXT NOT NULL,
-    description        TEXT NOT NULL,
-    starts_at          TIMESTAMPTZ,
-    ends_at            TIMESTAMPTZ,
-    recurrence_rule    JSONB,
+    id              UUID DEFAULT gen_random_uuid(),
+    owner_id        UUID NOT NULL,
+    name            TEXT NOT NULL,
+    description     TEXT NOT NULL,
+    starts_at       TIMESTAMPTZ,
+    ends_at         TIMESTAMPTZ,
+    recurrence_rule JSONB,
     PRIMARY KEY (id),
     FOREIGN KEY (owner_id) REFERENCES users (id)
+);
+
+CREATE TABLE event_overrides
+(
+    id          UUID DEFAULT gen_random_uuid(),
+    event_id    UUID NOT NULL,
+    ord_start   INT NOT NULL, -- nullable for from start modification?
+    ord_end     INT NOT NULL, -- nullable for to end modification?
+    name        TEXT,
+    description TEXT,
+    starts_at   TIMESTAMPTZ,
+    ends_at     TIMESTAMPTZ,
+    PRIMARY KEY (id),
+    FOREIGN KEY (event_id) REFERENCES events (id)
 );
 
 CREATE TABLE user_events
