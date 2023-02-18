9e511183aa639e40610dc923b1c30029e81c38bd
HubertK05 <hubert.klimowicz.hk@gmail.com>
2023-02-18 12:15:12 +0100

    🧪 add event calculation tests

--- backend/src/utils/events/modification.rs
+++ backend/src/utils/events/modification.rs
@@ -309,7 +309,7 @@ fn nth_next_month(val: Month, chg: u32) -> anyhow::Result<Month> {
 }
 
 fn days_between_two_weekdays(val_a: Weekday, val_b: Weekday) -> u8 {
-    (val_b.number_from_monday() - val_a.number_from_monday()) % 7
+    (((val_b.number_from_monday() as i8) - (val_a.number_from_monday() as i8)) % 7) as u8
 }
 
 fn year_is_by_day_count_to_until(
@@ -405,4 +405,193 @@ fn year_count_to_until(
         }
     }
     Ok(None) // never
+}
+
+mod recurrence_tests {
+    use uuid::Uuid;
+
+    use crate::utils::events::models::Event;
+
+    use super::*;
+
+    fn create_test_event_part(
+        event_starts_at: OffsetDateTime,
+        event_ends_at: OffsetDateTime,
+        recurrence_rule: EventRules,
+        part_starts_at: OffsetDateTime,
+        part_length: RecurrenceEndsAt,
+    ) -> EventPart {
+        EventPart {
+            event_data: Event {
+                id: Uuid::new_v4(),
+                owner_id: Uuid::new_v4(),
+                name: "Test event".into(),
+                starts_at: Some(event_starts_at),
+                ends_at: Some(event_ends_at),
+                recurrence_rule: Some(Json(recurrence_rule)),
+                description: "Test description".into(),
+            },
+            part_starts_at,
+            part_length: Some(part_length),
+        }
+    }
+
+    #[test]
+    fn daily_recurrence_test() {
+        let data = create_test_event_part(
+            datetime!(2023-02-18 10:00 +1),
+            datetime!(2023-02-18 12:15 +1),
+            EventRules::Daily { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(15)), interval: 3 } },
+            datetime!(2023-02-21 10:00 +1),
+            RecurrenceEndsAt::Count(7)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2023-03-14 12:15 +1)))
+    }
+
+    #[test]
+    fn weekly_recurrence_test() {
+        let data = create_test_event_part(
+            datetime!(2023-02-15 10:00 +1),
+            datetime!(2023-02-15 12:15 +1),
+            EventRules::Weekly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(30)), interval: 2 }, week_map: 86 },
+            datetime!(2023-02-27 10:00 +1),
+            RecurrenceEndsAt::Count(5)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2023-03-15 12:15 +1)))
+    }
+
+    #[test]
+    fn weekly_recurrence_test_next_week_offset() {
+        let data = create_test_event_part(
+            datetime!(2023-02-15 10:00 +1),
+            datetime!(2023-02-15 12:15 +1),
+            EventRules::Weekly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(30)), interval: 2 }, week_map: 86 },
+            datetime!(2023-03-01 10:00 +1),
+            RecurrenceEndsAt::Count(7)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2023-03-27 12:15 +1)))
+    }
+
+    #[test]
+    fn monthly_recurrence_test_by_day() {
+        let data = create_test_event_part(
+            datetime!(2023-02-18 10:00 +1),
+            datetime!(2023-02-18 12:15 +1),
+            EventRules::Monthly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 2 }, is_by_day: true },
+            datetime!(2023-04-18 10:00 +1),
+            RecurrenceEndsAt::Count(2)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2023-08-18 12:15 +1)))
+    }
+
+    #[test]
+    fn monthly_recurrence_test_by_day_month_end() {
+        let data = create_test_event_part(
+            datetime!(2025-01-29 10:00 +1),
+            datetime!(2025-01-29 12:15 +1),
+            EventRules::Monthly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(30)), interval: 5 }, is_by_day: true },
+            datetime!(2025-11-29 10:00 +1),
+            RecurrenceEndsAt::Count(15),
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2032-07-29 12:15 +1)))
+    }
+
+    #[test]
+    fn monthly_recurrence_test_by_weekday() {
+        let data = create_test_event_part(
+            datetime!(2023-02-18 10:00 +1),
+            datetime!(2023-02-18 12:15 +1),
+            EventRules::Monthly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 2 }, is_by_day: false },
+            datetime!(2023-04-15 10:00 +1),
+            RecurrenceEndsAt::Count(2)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2023-08-19 12:15 +1)))
+    }
+
+    #[test]
+    fn monthly_recurrence_test_by_weekday_month_end() {
+        let data = create_test_event_part(
+            datetime!(2023-01-31 10:00 +1),
+            datetime!(2023-01-31 12:15 +1),
+            EventRules::Monthly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 1 }, is_by_day: false },
+            datetime!(2023-05-30 10:00 +1),
+            RecurrenceEndsAt::Count(2),
+        );
+        // 1 - May 30
+        // 2 - August 29
+        // 3 - October 31
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2023-10-31 12:15 +1)))
+    }
+
+    #[test]
+    fn yearly_recurrence_test_by_day() {
+        let data = create_test_event_part(
+            datetime!(2023-02-18 10:00 +1),
+            datetime!(2023-02-18 12:15 +1),
+            EventRules::Yearly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 2 }, is_by_day: true },
+            datetime!(2025-02-18 10:00 +1),
+            RecurrenceEndsAt::Count(2)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2029-02-18 12:15 +1)))
+    }
+
+    #[test]
+    fn yearly_recurrence_test_by_day_feb_29() {
+        let data = create_test_event_part(
+            datetime!(2024-02-29 10:00 +1),
+            datetime!(2024-02-29 12:15 +1),
+            EventRules::Yearly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 1 }, is_by_day: true },
+            datetime!(2028-02-29 10:00 +1),
+            RecurrenceEndsAt::Count(1)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2032-02-29 12:15 +1)))
+    }
+
+    #[test]
+    fn yearly_recurrence_test_by_weekday() {
+        let data = create_test_event_part(
+            datetime!(2023-02-18 10:00 +1),
+            datetime!(2023-02-18 12:15 +1),
+            EventRules::Yearly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 2 }, is_by_day: false },
+            datetime!(2025-02-15 10:00 +1),
+            RecurrenceEndsAt::Count(2)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2029-02-17 12:15 +1)))
+    }
+
+    #[test]
+    fn yearly_recurrence_test_by_weekday_52nd_week() {
+        let data = create_test_event_part(
+            datetime!(2020-12-26 10:00 +1),
+            datetime!(2020-12-26 12:15 +1),
+            EventRules::Yearly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 1 }, is_by_day: false },
+            datetime!(2022-01-01 10:00 +1),
+            RecurrenceEndsAt::Count(1)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2022-12-31 12:15 +1)))
+    }
+
+    #[test]
+    fn yearly_recurrence_test_by_weekday_53rd_week() {
+        let data = create_test_event_part(
+            datetime!(2020-12-30 10:00 +1),
+            datetime!(2020-12-30 12:15 +1),
+            EventRules::Yearly { time_rules: TimeRules { ends_at: Some(RecurrenceEndsAt::Count(10)), interval: 1 }, is_by_day: false },
+            datetime!(2026-12-31 10:00 +1),
+            RecurrenceEndsAt::Count(1)
+        );
+
+        assert_eq!(data.count_to_until().unwrap(), Some(datetime!(2032-12-30 12:15 +1)))
+    }
 }
