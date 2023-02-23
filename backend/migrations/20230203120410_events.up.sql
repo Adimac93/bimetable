ef22c3b67bb67b6f15772e26ecb82edadd2b79ec
Adimac93 <adimac93@gmail.com>
2023-02-23 16:28:21 +0100

    🗃️ events + timestamp based overrides + temporary deletion by `deleted_at`

--- backend/migrations/20230203120410_events.up.sql
+++ backend/migrations/20230203120410_events.up.sql
@@ -1,35 +1,40 @@
 CREATE TABLE events
 (
     id              UUID DEFAULT gen_random_uuid(),
-    owner_id        UUID NOT NULL,
-    name            TEXT NOT NULL,
-    description     TEXT NOT NULL,
-    starts_at       TIMESTAMPTZ,
-    ends_at         TIMESTAMPTZ,
+    owner_id        UUID        NOT NULL,
+    name            TEXT        NOT NULL,
+    description     TEXT,
+    starts_at       TIMESTAMPTZ NOT NULL,
+    ends_at         TIMESTAMPTZ NOT NULL,
+    deleted_at      TIMESTAMPTZ,
     recurrence_rule JSONB,
     PRIMARY KEY (id),
     FOREIGN KEY (owner_id) REFERENCES users (id)
 );
 
+
 CREATE TABLE event_overrides
 (
-    id          UUID DEFAULT gen_random_uuid(),
-    event_id    UUID NOT NULL,
-    ord_start   INT NOT NULL, -- nullable for from start modification?
-    ord_end     INT NOT NULL, -- nullable for to end modification?
-    name        TEXT,
-    description TEXT,
-    starts_at   TIMESTAMPTZ,
-    ends_at     TIMESTAMPTZ,
+    id                 UUID                 DEFAULT gen_random_uuid(),
+    event_id           UUID        NOT NULL,
+    override_starts_at TIMESTAMPTZ NOT NULL,
+    override_ends_at   TIMESTAMPTZ NOT NULL,
+    created_at         TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP AT TIME ZONE 'UTC',
+    name               TEXT,
+    description        TEXT,
+    starts_at          TIMESTAMPTZ,
+    ends_at            TIMESTAMPTZ,
+    deleted_at         TIMESTAMPTZ,
     PRIMARY KEY (id),
     FOREIGN KEY (event_id) REFERENCES events (id)
 );
 
 CREATE TABLE user_events
 (
-    user_id  UUID NOT NULL,
-    event_id UUID NOT NULL,
-    can_edit BOOL NOT NULL,
+    user_id     UUID NOT NULL,
+    event_id    UUID NOT NULL,
+    can_edit    BOOL NOT NULL,
+    is_accepted BOOL NOT NULL DEFAULT FALSE,
     PRIMARY KEY (user_id, event_id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (event_id) REFERENCES events (id)
