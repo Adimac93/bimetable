1d67208dbf73580de3473f4258a8037e983b54bd
Mabi19 <mateuszbartnik33@gmail.com>
2023-02-23 18:58:00 +0100

    🔨 Add default value for API URL

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ import settings from "./configuration/settings.json";
 export default defineNuxtConfig({
     // these may be overwritten by env vars
     appConfig: {
-        apiURL: settings.apiURL
+        apiURL: settings.apiURL ?? "http://localhost:3001"
     },
     css: [
         "assets/global.scss"
